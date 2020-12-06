cb901afbcb92b1b0ee1f5955dcae3f79693c39ce
erathe <eirik.rathe@gmail.com>
2020-12-06 09:22:28 +0100

    naming day 6

--- 6.py
+++ 6.py
@@ -6,4 +6,4 @@ lines = [line.strip() for line in open("6.in").read().split("\n\n")]
 print(sum([len(set(line.replace("\n", ""))) for line in lines]))
 
 #part 2
-print(len([c for line in lines for c, i in Counter(line).items() if i == len(line.split("\n"))]))
+print(len([c for block in lines for c, i in Counter(block).items() if i == len(block.split("\n"))]))
