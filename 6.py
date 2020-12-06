eea821025de1f19cd6bc423addb2d8a641814dc9
erathe <eirik.rathe@gmail.com>
2020-12-06 11:46:28 +0100

    remove unecessary list comprehension

--- 6.py
+++ 6.py
@@ -3,7 +3,7 @@ from collections import Counter
 lines = [line.strip() for line in open("6.in").read().split("\n\n")]
 
 #part 1
-print(sum([len(set(line.replace("\n", ""))) for line in lines]))
+print(sum(len(set(line.replace("\n", ""))) for line in lines))
 
 #part 2
 print(len([c for block in lines for c, i in Counter(block).items() if i == len(block.split("\n"))]))
