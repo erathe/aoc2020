72af6d2ed8af1d9f0d20d006f3d4f9c651b453f2
erathe <eirik.rathe@gmail.com>
2020-12-08 09:15:09 +0100

    day 8

--- 8.py
+++ 8.py
@@ -23,22 +23,38 @@ def run_boot(commands):
 print(run_boot(inst)[1])
 
 #part 2
-complete_instructions = []
+changes = {
+        "jmp": "nop",
+        "nop": "jmp"
+        }
 for i, line in enumerate(inst):
-    if line[0:3] == "jmp":
-        copy = inst.copy()
-        copy[i] = "nop" + line[3:]
-        #Could have checked the list here to avoid generating all variations
-        complete_instructions.append(copy)
-        continue
-    if line[0:3] == "nop":
-        copy = inst.copy()
-        copy[i] = "jmp" + line[3:]
-        complete_instructions.append(copy)
-        continue
+    if line[0:3] in changes.keys():
+        cp = inst[i]
+        inst[i] = inst[i].replace(line[0:3], changes[line[0:3]])
+        success, accumulator = run_boot(inst)
+        if success:
+            print(accumulator)
+            break
+        inst[i] = cp
 
-for line in complete_instructions:
-    success, accumulator = run_boot(line)
-    if success:
-        print(accumulator)
-        break
+#first go at part 2
+#complete_instructions = []
+#for i, line in enumerate(inst):
+#    if line[0:3] == "jmp":
+#        copy = inst.copy()
+#        copy[i] = "nop" + line[3:]
+#        #Could have checked the list here to avoid generating all variations
+#        #It is also probably faster to change the value, check and then change it back
+#        complete_instructions.append(copy)
+#        continue
+#    if line[0:3] == "nop":
+#        copy = inst.copy()
+#        copy[i] = "jmp" + line[3:]
+#        complete_instructions.append(copy)
+#        continue
+#
+#for line in complete_instructions:
+#    success, accumulator = run_boot(line)
+#    if success:
+#        print(accumulator)
+#        break
